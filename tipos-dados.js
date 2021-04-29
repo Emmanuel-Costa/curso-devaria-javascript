d29b228e2446c1ad4855131f3c73d6fd7c53c16e
Emmanuel-Costa <emmanuelcosta@id.uff.br>
2021-04-29 17:28:45 -0300

    Aula 21 javascript Devaria

---
+++ tipos-dados.js
@@ -0,0 +1,28 @@
+const inteiro = 10;
+const decimal = 10.5;
+const bool = false;
+const nome = "Matheus";
+let copo = null;
+let notebook = undefined;
+let pessoa = 
+{
+    nome: "Daniel",
+    idade: 28
+}
+const symb = Symbol()
+
+console.log("Tipo da variavel inteiro: ",typeof inteiro);
+console.log("Tipo da variavel decimal: ",typeof decimal);
+console.log("Tipo da variavel bool: ",typeof bool);
+console.log("Tipo da variavel nome: ",typeof nome);
+console.log("Tipo da variavel copo: ",typeof copo);
+console.log("Tipo da variavel notebook: ",typeof notebook);
+console.log("Tipo da variavel pessoa: ",typeof pessoa);
+console.log("Tipo da variavel symb: ",typeof symb);
+
+
+
+
+
+
+
